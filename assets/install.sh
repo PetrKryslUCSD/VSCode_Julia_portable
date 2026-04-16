26f9e4dd688fe38159c7b3ec556a55c4f07b38f3
Petr Krysl <pkrysl@ucsd.edu>
2026-04-16 08:35:46 -0700

    update script

--- assets/install.sh
+++ assets/install.sh
@@ -26,7 +26,7 @@ set -o nounset
 
 # Select the version of julia to run
 MyPortableJuliaMajorVersion=1.12
-MyPortableJuliaMinorVersion=.4 # can be used to also select release candidate
+MyPortableJuliaMinorVersion=.6 # can be used to also select release candidate
 
 MyPortableJulia=julia-$MyPortableJuliaMajorVersion$MyPortableJuliaMinorVersion
 
@@ -58,18 +58,6 @@ export JULIA_DEPOT_PATH="$MyDepot"
 # We want to find executables in the julia depot
 export PATH=$JULIA_DEPOT_PATH/bin:$PATH
 
-# Expand the other zip files
-# if [ ! -d assets/gnuplot ] ; then
-#     if [ ! -f ./assets/gnuplot.zip ] ; then
-#         echo "Downloading gnuplot"
-#         curl http://tmacchant33.starfree.jp/gnuplot_files/gp550-20220416-win64-mingw.zip --output ./assets/gnuplot.zip
-#     fi
-#     echo Installing gnuplot 
-#     cd assets
-#     unzip -q ./gnuplot.zip
-#     cd ..
-# fi
-
 # Make sure we can start Julia just by referring to the program name.
 export PATH="$(pwd)"/assets/$MyPortableJulia/bin:$PATH
 
@@ -78,21 +66,6 @@ export PATH="$(pwd)"/assets/$MyPortableJulia/bin:$PATH
 
 # Add the Git binary
 export PATH="$(pwd)"/assets/PortableGit/bin:$PATH
-
-# Download, and instantiate the tutorial packages, in order to bring Julia depot up-to-date 
-# if [ ! -d JuliaTutorial ] ; then
-#     echo "Activating/instantiating a package"
-#     for n in JuliaTutorial
-#     do 
-#         if [ ! -d $n ] ; then
-#             echo "Activating and instantiating $n"
-#             git clone https://github.com/PetrKryslUCSD/$n.git
-#         fi
-#         cd $n
-#         julia -e 'using Pkg; Pkg.activate("."); Pkg.instantiate(); Pkg.precompile(); exit()'
-#         cd ..
-#     done
-# fi
 
 # Make sure the Julia REPL when started activates/instantiates
 if [ ! -f "$MyDepot"/config/startup.jl ] ; then
@@ -107,33 +80,18 @@ cat<<EOF >> "$MyDepot/config/startup.jl"
 using Pkg 
 # Disable updating registry on add (still runs on up), as it is slow
 Pkg.UPDATED_REGISTRY_THIS_SESSION[] = true
-# The below would only be useful when not using VS Code: running REPL in VS Code does it automatically
-# Globally useful packages
-# import Pkg; Pkg.add("Revise")
-# import Pkg; Pkg.add("JuliaFormatter")
-# Setup global packages
-# atreplinit() do repl
-#     try
-#         @eval using Revise
-#     catch err
-#         println("Error starting Revise: \$err")
-#     end
-#     try
-#         @eval using JuliaFormatter
-#     catch err
-#         println("Error starting JuliaFormatter: \$err")
-#     end
-# end
-# Is this a project environment?
-# if isfile("Project.toml") && isfile("Manifest.toml")
-#    Pkg.activate(".") 
-#    Pkg.instantiate()
-# end
-# Better format for floats
-# using Printf 
-# Base.show(io::IO, f::Float64) = @printf(io, "%1.5e", f)
-# Base.show(io::IO, f::Float32) = @printf(io, "%1.5e", f)
-# Base.show(io::IO, f::Float16) = @printf(io, "%1.5e", f)
+atreplinit() do repl
+    try
+        @eval using Revise
+    catch err
+        println("Error starting Revise: \$err")
+    end
+    try
+        @eval using JuliaFormatter
+    catch err
+        println("Error starting JuliaFormatter: \$err")
+    end
+end
 EOF
 fi
 
@@ -156,20 +114,23 @@ else
     echo "Found VSCode"
 fi
 
-# Install required extensions
+# Install required/useful extensions
 if [ ! -f assets/firsttimedone ] ; then
     if [ ! -d assets/VSCode/data ] ; then
-	mkdir assets/VSCode/data
+	    mkdir assets/VSCode/data
     fi
     assets/VSCode/bin/code --install-extension alefragnani.Bookmarks --force
     assets/VSCode/bin/code --install-extension julialang.language-julia --force
     assets/VSCode/bin/code --install-extension kaiwood.center-editor-window --force
-    #assets/VSCode/bin/code --install-extension stkb.rewrap --force
+    assets/VSCode/bin/code --install-extension TroelsDamgaard.reflow-paragraph --force
     assets/VSCode/bin/code --install-extension yeannylam.recenter-top-bottom --force
     assets/VSCode/bin/code --install-extension nemesv.copy-file-name --force
     assets/VSCode/bin/code --install-extension PKief.material-icon-theme --force
     assets/VSCode/bin/code --install-extension johnpapa.vscode-peacock --force
     assets/VSCode/bin/code --install-extension chunsen.bracket-select --force
+    assets/VSCode/bin/code --install-extension pokey.command-server --force
+    assets/VSCode/bin/code --install-extension pokey.talon --force
+    assets/VSCode/bin/code --install-extension sleistner.vscode-fileutils --force
     touch assets/firsttimedone
 fi
 
@@ -186,7 +147,7 @@ cat<<EOF >> $settings_file
         "language-julia.interrupt"
     ],
     "julia.symbolCacheDownload": true,
-    "window.title": "\${activeEditorShort}\${separator}\${rootName}\${separator}\${profileName}\${separator}focus:\${focusedView}",
+    "window.title": "\${activeEditorShort}\${separator}\${rootName}\${separator}\${profileName}\${separator}focus:[\${focusedView}]\${separator}lang:[\${activeEditorLanguageId}]",
 }
 EOF
 fi
